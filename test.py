b5399ec01c861906417ae6a83fc212dbe4d59d57
黄大仙 <wow93930@163.com>
2020-07-08 20:14:13 +0800

    print input

---
+++ test.py
@@ -0,0 +1,21 @@
+# !/usr/bin/python
+# _*_encoding: utf-8 _*_
+print("hello,world");
+print("这是我编写的第一个py脚本")
+print("你好啊,我的世界")
+if True:
+    print("这是 true")
+else:
+    print("这是 false")
+total = "item1" + \
+        "item1" + \
+        "item3"
+number = 1
+a = 2
+number = a + number
+print(total)
+print(number)
+"""为什么注释是这个样子的...有点反人类啊"""
+"""行吧.这下面就开始等待输入了:"""
+input_str=input("按下enter退出,其他的任意键显示 \n")
+print(input_str)
